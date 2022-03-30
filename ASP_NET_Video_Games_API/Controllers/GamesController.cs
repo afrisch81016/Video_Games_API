138102ed6e91af4b8180728935f2b26c21c9d9c2
unknown <andy.smith12@ymail.com>
2022-03-30 14:16:30 -0500

    removed stashed code

--- ASP_NET_Video_Games_API/Controllers/GamesController.cs
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -4,10 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Video_Games_API.Controllers
 {
-<<<<<<< Updated upstream
-    // api/examples
-=======
->>>>>>> Stashed changes
+
     [Route("api/[controller]")]
     [ApiController]
     public class GamesController : ControllerBase
@@ -18,17 +15,7 @@ namespace ASP_NET_Video_Games_API.Controllers
         {
             _context = context;
         }
-<<<<<<< Updated upstream
-        [HttpGet]
-        public IActionResult GetPublishers()
-        {
-            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
 
-            return Ok(videoGamePublishers);
-        }
-        [HttpGet("{pubName}")]
-        public IActionResult GetGamesbyPublisher(string pubName)
-=======
 
         [HttpGet]
         public IActionResult GetGames()
@@ -40,16 +27,12 @@ namespace ASP_NET_Video_Games_API.Controllers
 
         [HttpGet("{id}")]
         public IActionResult GetGamesById(int id)
->>>>>>> Stashed changes
         {
             int? maxYear = _context.VideoGames.Select(vg => vg.Year).Max();
             int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
 
-<<<<<<< Updated upstream
-            var videoGames =_context.VideoGames.Where(vg => vg.Publisher == pubName);
-=======
+
             var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
->>>>>>> Stashed changes
             return Ok(videoGames);
         }
     }
